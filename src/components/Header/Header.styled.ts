57f6f4a535988737623b66ab40ed8eb3c725753b
Mia-Salazar <maria.sgr@gmail.com>
2025-03-25 17:38:52 +0100

    Change header size

--- src/components/Header/Header.styled.ts
+++ src/components/Header/Header.styled.ts
@@ -4,6 +4,7 @@ import styled from "styled-components";
 export const TitleStyled = styled.h1`
     margin: 0;
     color: #fff;
+    font-size: 42px;
     font-family: "Jolly Lodger", system-ui;
 `
 
